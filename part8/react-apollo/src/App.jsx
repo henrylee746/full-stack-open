7c022688387a3db350832cb8823ff157e99bb3a8
henrylee746 <henry.lee746@gmail.com>
2025-11-15 19:23:20 -0500

    on 8.8

--- part8/react-apollo/src/App.jsx
+++ part8/react-apollo/src/App.jsx
@@ -1,25 +1,33 @@
-import { gql } from "@apollo/client";
 import { useQuery } from "@apollo/client/react";
 import Persons from "./components/Persons";
-
-const ALL_PERSONS = gql`
-  query {
-    allPersons {
-      name
-      phone
-      id
-    }
-  }
-`;
+import { ALL_PERSONS } from "./queries";
+import { useState } from "react";
+import PersonForm from "./components/PersonForm";
+import Notify from "./components/Notify";
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null);
+
   const result = useQuery(ALL_PERSONS);
 
   if (result.loading) {
     return <div>loading...</div>;
   }
 
-  return <Persons persons={result.data.allPersons} />;
+  const notify = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 10000);
+  };
+
+  return (
+    <div>
+      <Notify errorMessage={errorMessage} />
+      <Persons persons={result.data.allPersons} />
+      <PersonForm setError={notify} />
+    </div>
+  );
 };
 
 export default App;
