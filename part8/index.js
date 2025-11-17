837bae697448f4c5115441f30757f4a9b35fc04a
henrylee746 <henry.lee746@gmail.com>
2025-11-16 19:05:34 -0500

    On 8.13

--- part8/index.js
+++ part8/index.js
@@ -102,6 +102,9 @@ type Mutation {
     username: String!
     password: String!
   ): Token
+  addAsFriend(
+    name: String!
+  ): User
 }
 `;
 
@@ -129,10 +132,21 @@ const resolvers = {
     },
   },
   Mutation: {
-    addPerson: async (root, args) => {
+    addPerson: async (root, args, context) => {
       const person = new Person({ ...args });
+      const currentUser = context.currentUser;
+
+      if (!currentUser) {
+        throw new GraphQLError("not authenticated", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+          },
+        });
+      }
       try {
         await person.save();
+        currentUser.friends = currentUser.friends.concat(person);
+        await currentUser.save();
       } catch (error) {
         throw new GraphQLError("Saving person failed", {
           extensions: {
@@ -190,6 +204,27 @@ const resolvers = {
         });
       }
       return person;
+    },
+    addAsFriend: async (root, args, { currentUser }) => {
+      const isFriend = (person) =>
+        currentUser.friends
+          .map((f) => f._id.toString())
+          .includes(person._id.toString());
+
+      if (!currentUser) {
+        throw new GraphQLError("wrong credentials", {
+          extensions: { code: "BAD_USER_INPUT" },
+        });
+      }
+
+      const person = await Person.findOne({ name: args.name });
+      if (!isFriend(person)) {
+        currentUser.friends = currentUser.friends.concat(person);
+      }
+
+      await currentUser.save();
+
+      return currentUser;
     },
   },
 };
