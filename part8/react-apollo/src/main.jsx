eb45c5357e303ce6f4bbf4a1a9d57245d039b888
henrylee746 <henry.lee746@gmail.com>
2025-11-30 02:29:25 -0500

    Started FSO 8D

--- part8/react-apollo/src/main.jsx
+++ part8/react-apollo/src/main.jsx
@@ -1,8 +1,20 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { ApolloClient, InMemoryCache, HttpLink, gql } from "@apollo/client";
 import { ApolloProvider } from "@apollo/client/react";
+import { setContext } from "@apollo/client/link/context";
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("phonenumbers-user-token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null,
+    },
+  };
+});
 
 const client = new ApolloClient({
   link: new HttpLink({ uri: "http://localhost:4000" }),
